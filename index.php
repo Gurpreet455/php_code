586b926bf1d96acd5ab04baebcbab47c485e5ab7
Gurpreet Singh <117374055+Gurpreet455@users.noreply.github.com>
2024-05-23 23:54:00 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document25</title>
+    <title>Document26</title>
 </head>
 <body>
     hello this is php code here
